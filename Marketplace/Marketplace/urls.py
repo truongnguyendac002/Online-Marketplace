a2d976242877499c94324dbf392793da7a635bc0
truongnguyendac002 <truongnguyendac002@gmail.com>
2023-11-15 10:14:48 +0700

    update - sign up

--- Marketplace/Marketplace/urls.py
+++ Marketplace/Marketplace/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
+from core import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('contact', contact, name='contact'),
-    
+    path('', include('core.urls')),
+    path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
